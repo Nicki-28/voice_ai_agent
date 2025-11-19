8d65979c1df45d085f01a68e65480b97c260d5bf
nicole <nickiissj@gmail.com>
2025-11-19 09:25:30 +0100

    integrando la logica

--- core/stt.py
+++ core/stt.py
@@ -6,28 +6,29 @@ import json
 palabra_detectada = False
 query_list=[]
 
-def text_detected(text):
-    global palabra_detectada
-    global query_list
-    
-    texto = text.strip().lower()
-    if not palabra_detectada:
-            if "hey jarvis" in texto:
-                palabra_detectada = True
-                query_list.append(texto)    
-            return
-    query_list.append(texto)
-            
-#guardamos en un json 
-    with open("query.json", "w", encoding="utf-8") as f:
-        json.dump({"content": " ".join(query_list)}, f, indent=4, ensure_ascii=False)
-        
-if __name__ == "__main__":
-    print("*Escuchando...*")
-    
-    with AudioToTextRecorder(model="tiny") as recorder:
-        while True:
-            recorder.text(text_detected)
-           
+def start_listening ():
 
-   
+    def text_detected(text):
+        global palabra_detectada
+        global query_list
+        
+        texto = text.strip().lower()
+        if not palabra_detectada:
+                if "hey jarvis" in texto:
+                    palabra_detectada = True
+                    query_list.append(texto)    
+                return
+        query_list.append(texto)
+                
+    #guardamos en un json 
+        with open("query.json", "w", encoding="utf-8") as f:
+            json.dump({"content": " ".join(query_list)}, f, indent=4, ensure_ascii=False)
+            
+        with AudioToTextRecorder(model="tiny") as recorder:
+            while not palabra_detectada:
+                recorder.text(text_detected)
+            print("Wake word detectada, empieza a transcribir!")
+    return " ".join(query_list)
+            
+
+    
