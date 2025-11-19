be7e1f4364be874d6e97f6fd50bdf156e6ea4a04
nicole <nickiissj@gmail.com>
2025-11-19 09:37:40 +0100

    mal indentado

--- core/stt.py
+++ core/stt.py
@@ -1,34 +1,33 @@
 from RealtimeSTT import AudioToTextRecorder
-import pyautogui
 import json
-#quiero que empiece a escuchar cuando escuche la wakeup word
 
 palabra_detectada = False
-query_list=[]
+query_list = []
 
-def start_listening ():
+def start_listening():
 
     def text_detected(text):
-        global palabra_detectada
-        global query_list
-        
+        global palabra_detectada, query_list
+
         texto = text.strip().lower()
+
+        #Detectamos la wakeup word
         if not palabra_detectada:
-                if "hey jarvis" in texto:
-                    palabra_detectada = True
-                    query_list.append(texto)    
-                return
+            if "hey jarvis" in texto:
+                print("he detectado la palabra, debo escribir lo que dices")
+                palabra_detectada = True
+            return  
+
+       #guardar todo lo que diga
         query_list.append(texto)
-                
-    #guardamos en un json 
+
+        
         with open("query.json", "w", encoding="utf-8") as f:
             json.dump({"content": " ".join(query_list)}, f, indent=4, ensure_ascii=False)
-            
-        with AudioToTextRecorder(model="tiny") as recorder:
-            while not palabra_detectada:
-                recorder.text(text_detected)
-            print("Wake word detectada, empieza a transcribir!")
-    return " ".join(query_list)
-            
 
-    
+    with AudioToTextRecorder(model="tiny") as recorder:
+        print("Escuchando wake word...")
+        while True:
+            recorder.text(text_detected)
+
+    return " ".join(query_list)
