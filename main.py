d366d5deee070c92d82ca5befa9e8004ae44e1d3
nicole <nickiissj@gmail.com>
2025-11-19 09:19:14 +0100

    si, porque sino meto esto no me entero de na

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from core.api_request import obtener_respuesta
 
 while True:
     user_text= start_listening()
+    print("ESTOY ESCUCHANDO, di hey jarvis")
     
     respuesta = obtener_respuesta(user_text)
     
