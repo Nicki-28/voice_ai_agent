25430c3ddbb27c6fc92dd668c641ff3a83f7ccc5
nicole <nickiissj@gmail.com>
2025-11-20 20:52:43 +0100

    using Silero VAD for better silence detection, it's significably shorter now, but not quite my tempo

--- core/stt.py
+++ core/stt.py
@@ -5,9 +5,6 @@ import time
 palabra_detectada = False
 query_list = []
 
-# Tiempo máximo de silencio en segundos antes de finalizar la captura
-SILENCE_TIMEOUT = 1.0
-
 def start_listening(timeout_silence=0.5):
     global palabra_detectada, query_list
 
@@ -20,9 +17,10 @@ def start_listening(timeout_silence=0.5):
         texto = text.strip().lower()
         current_time = time.time()
 
-        # Detectar wake wordcl
+        # wake word
         if not palabra_detectada:
             if "hey jarvis" in texto:
+                #DEBUGGING
                 print("Wake word detectada, empiezo a transcribir...")
                 palabra_detectada = True
                 last_heard_time = current_time
@@ -33,18 +31,20 @@ def start_listening(timeout_silence=0.5):
             query_list.append(texto)
             last_heard_time = current_time
 
-        # Guardar en JSON cada vez que se recibe algo
+        
         with open("query.json", "w", encoding="utf-8") as f:
             json.dump({"content": " ".join(query_list)}, f, indent=4, ensure_ascii=False)
 
     with AudioToTextRecorder(
             model="tiny",
             post_speech_silence_duration=0.2,
+            silero_deactivity_detection= True
         ) as recorder:
             while True:
                 recorder.text(text_detected)
                 if palabra_detectada and last_heard_time:
                     if time.time() - last_heard_time > timeout_silence:
+                        #DEBUGGING
                         print("Silencio detectado, terminando captura.")
                         break
 
