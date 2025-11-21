9c86d4035e77186d49f2521dbcc76f347a61d718
nicole <nickiissj@gmail.com>
2025-11-21 19:03:44 +0100

    arreglar ahora que puse el yield

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from core.api_request import obtener_respuesta
 
 
 def run():
-    user_text = start_listening()
-    print("Heard:", user_text)
-    respuesta = obtener_respuesta(user_text)
-    print("AI:", respuesta)
+    for user_text in start_listening():
+        print("Heard:", user_text)
+        respuesta = obtener_respuesta(user_text)
+        print("AI:", respuesta)
 
 if __name__ == "__main__":
     run()
