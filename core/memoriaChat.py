526bb177ed3fa24ef9b062a03ee006b0b89d3a76
nicole <nickiissj@gmail.com>
2026-02-17 12:48:53 +0100

    feat: adapt idea of the chat having a memory

--- core/memoriaChat.py
+++ core/memoriaChat.py
@@ -1,14 +1,13 @@
 import os
 import json
 
-MEMORY_PATH = "context.json"
 
 def cargar_memoria():
-    if not os.path.exists(MEMORY_PATH):
+    if not os.path.exists("output.json"):
         return []
-    with open(MEMORY_PATH, "r") as f:
+    with open("output.json", "r") as f:
         return json.load(f)
 
 def guardar_memoria(historial):
-    with open(MEMORY_PATH, "w") as f:
+    with open("context.json", "w") as f:
         json.dump(historial, f, indent=2)
