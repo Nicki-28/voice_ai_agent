1d127225b77f394c7589c2a920bd921f4bc3b76e
nicole <nickiissj@gmail.com>
2026-06-13 01:40:36 +0200

    feat: redo of the whole tts logic into a websocket one

--- core/tts.py
+++ core/tts.py
@@ -1,25 +1,41 @@
-from config import API_KEY_ELEVEN
-from dotenv import load_dotenv
-from elevenlabs.client import ElevenLabs
-from elevenlabs.play import play
-import os
+import subprocess
+import requests
+import json
+import base64
+from config import INWORLD_API_KEY
 
-load_dotenv()
+def sintetizar_voz_inworld_stream(texto):
+    url = "https://api.inworld.ai/tts/v1/voice:stream"
+    headers = {
+        "Authorization": f"Basic {INWORLD_API_KEY}",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "text": texto,
+        "voiceId": "Ashley", 
+        "modelId": "inworld-tts-1.5-mini" 
+    }
+    
 
-def sintetizar_voz(texto):
-   
-    elevenlabs = ElevenLabs(
-    api_key=os.getenv(API_KEY_ELEVEN),
+    reproductor = subprocess.Popen(
+        ["mpv", "--no-cache", "--no-terminal", "--", "-"],
+        stdin=subprocess.PIPE
     )
-
-    audio = elevenlabs.text_to_speech.convert(
-        text=texto,
-        voice_id="JBFqnCBsd6RMkjVDRZzb",
-        model_id="eleven_multilingual_v2",
-        output_format="mp3_44100_128",
-    )
-
-    play(audio)
-
-  
-
+    
+    # petición iworld
+    response = requests.post(url, headers=headers, json=payload, stream=True)
+    
+    # procesamos 
+    for line in response.iter_lines():
+        if line:
+            chunk = json.loads(line)
+            if "result" in chunk and "audioContent" in chunk["result"]:
+                audio_data = base64.b64decode(chunk["result"]["audioContent"])
+                
+                reproductor.stdin.write(audio_data)
+                reproductor.stdin.flush() 
+                print(f"Reproduciendo fragmento MP3: {len(audio_data)} bytes")
+    
+    reproductor.stdin.close()
+    reproductor.wait()
+    print("Jarvis ha terminado de hablar.")
