b27a98fb398bdc3fad4561828bc6ada7c58c75cb
nicole <nickiissj@gmail.com>
2026-06-18 13:59:08 +0200

    feat: change from mpv to pyaudio, add multithreading

--- core/memoriaChat.py
+++ core/memoriaChat.py
@@ -5,21 +5,22 @@ import json
 def cargar_memoria():
     historial_completo = []
     
-    # 1. Cargamos el primer archivo (si existe)
+    # Cargamos el primer archivo (si existe)
     if os.path.exists("data/output.json"):
         with open("data/output.json", "r") as f:
             datos_output = json.load(f)
             historial_completo.extend(datos_output) # Añadimos los datos a la lista
             
-    # 2. Cargamos el segundo archivo (si existe)
+    # Cargamos el segundo archivo (si existe)
     if os.path.exists("data/context.json"):
         with open("data/context.json", "r") as g:
             datos_context = json.load(g)
             historial_completo.extend(datos_context) # Sumamos estos datos a la misma lista
             
-    # 3. return último
+    # return último
     return historial_completo
 
 def guardar_memoria(historial):
+    print("Guardando historial...")
     with open("data/context.json", "w") as f:
         json.dump(historial, f, indent=2)
